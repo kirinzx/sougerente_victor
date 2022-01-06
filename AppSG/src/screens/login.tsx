8b2a16a202340e1fe9f1445dd607fc3b13544bf7
Victor Pavani <77660524+kirinzx@users.noreply.github.com>
2022-01-06 12:11:52 -0300

    updt loginscreen

--- AppSG/src/screens/login.tsx
+++ AppSG/src/screens/login.tsx
@@ -15,14 +15,26 @@ const isAndroid = Platform.OS === 'android';
 const Profile = () => {
   const navigation = useNavigation();
   const { assets, colors, sizes } = useTheme();
-  const [Nome, SetNome] = useState({});
+  const [user, SetUser] = useState('');
+  const [senha, SetSenha] = useState('');
+  const [usuario, SetUsuario] = useState({});
+  const [hide, SetHide] = useState(true);
 
+  const getUser = async () => {
+    const { data } = await axios.get(`http://192.168.1.6/8LIGHT/api_sougerente/index.php/load_usuario_login?p1=${user}&p2=${senha}`);
+    SetUsuario(data[0]);
+    alert(usuario.nome_completo)
+    alert(usuario.cpf);
+  };
 
 
 
 
   return (
-    <KeyboardAvoidingView style={stilos.background}>
+    <KeyboardAvoidingView style={stilos.background}
+      behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={0}
+    >
       <View style={stilos.logoContainer}>
         <Image
           style={stilos.logo}
@@ -31,23 +43,41 @@ const Profile = () => {
         />
       </View>
 
+
+
       <View style={stilos.container}>
+        <Text style={stilos.welcome}>BEM-VINDO</Text>
+        <Text style={stilos.tituloUser}>Usuário</Text>
         <TextInput
           style={stilos.placeholder}
-          placeholder="Email"
+          placeholder="Entre usando CPF, Email ou Telefone"
           autoCorrect={false}
-          onChangeText={() => { }}
+          onChangeText={SetUser}
         />
 
+        <Text style={stilos.tituloSenha}>Senha</Text>
         <TextInput
           style={stilos.placeholder}
+          secureTextEntry={hide}
           placeholder="Senha"
           autoCorrect={false}
-          onChangeText={() => { }}
+          onChangeText={SetSenha}
         />
+        <TouchableOpacity onPress={() => SetHide(!hide)} style={{ marginLeft: '70%' }}>
+          <Image
+            style={stilos.eye}
+            source={require('../assets/images/olho.png')}
+          />
+        </TouchableOpacity>
 
-        <TouchableOpacity style={stilos.btnEntrar}>
-          <Text>Entrar</Text>
+        <TouchableOpacity>
+          <Text style={stilos.password}>Esqueci a Senha</Text>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity style={stilos.btnEntrar} onPress={getUser}>
+          <Text style={stilos.entrar}
+          >ENTRAR</Text>
         </TouchableOpacity>
 
       </View>
@@ -88,6 +118,11 @@ const stilos = StyleSheet.create({
     marginLeft: '-10%'
   },
 
+  eye: {
+    width: 25,
+    height: 25,
+  },
+
   container: {
     backgroundColor: '#CB8D00',
     flex: 1,
@@ -96,25 +131,75 @@ const stilos = StyleSheet.create({
     width: '100%',
     borderTopLeftRadius: 80,
     borderTopRightRadius: 80,
+    overflow: 'hidden',
   },
 
   placeholder: {
     fontSize: 18,
     fontWeight: 'bold',
     fontFamily: 'OpenSans-Bold',
-    marginTop: '8%',
+    marginTop: '4%',
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    width: '80%',
+    marginBottom: '4%',
   },
 
   btnEntrar: {
-    backgroundColor: 'white',
-    width: 70,
+    backgroundColor: '#EEA500',
+    width: 100,
     height: 40,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 25,
     marginTop: '10%',
-
-
+    borderWidth: 1,
+    borderColor: '#707070',
   },
 
+  tituloSenha: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'OpenSans-ExtraBold',
+    alignContent: 'center',
+    alignItems: 'center',
+    marginRight: '67%',
+  },
+
+  tituloUser: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'OpenSans-ExtraBold',
+    alignContent: 'center',
+    alignItems: 'center',
+    marginRight: '64%',
+    marginTop: '10%',
+  },
+
+  welcome: {
+    color: 'white',
+    fontSize: 23,
+    fontWeight: 'bold',
+    fontFamily: 'OpenSans-ExtraBold',
+    marginTop: '-3%',
+    marginRight: '52%',
+  },
+
+  entrar: {
+    color: 'white',
+    fontSize: 22,
+    fontFamily: 'OpenSans-Bold',
+    fontWeight: 'bold',
+  },
+
+  password: {
+    color: 'white',
+    fontSize: 16,
+    fontFamily: 'OpenSans-Bold',
+    fontWeight: 'bold',
+    marginRight: '55%',
+    marginTop: '-8%',
+  },
 });

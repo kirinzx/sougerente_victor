110dbf130d51da5f7126525764ff826fbf0c2eee
Victor Pavani <77660524+kirinzx@users.noreply.github.com>
2022-01-19 16:39:00 -0300

    updt victor pavani

--- AppSG/src/navigation/Screens.tsx
+++ AppSG/src/navigation/Screens.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import {Home, Profile} from '../screens';
+import { Home, Profile } from '../screens';
 
-import {useScreenOptions, useTranslation} from './hooks';
-import {TarefasAdmin} from '../screens/TarefasAdmin';
+import { useScreenOptions, useTranslation } from './hooks';
+import { TarefasAdmin } from '../screens/TarefasAdmin';
 import HomeGerente from '../screens/HomeGerente';
 import TarefasGerente from '../screens/TarefasGerente';
 import HomeADM from '../screens/HomeADM';
 import agenda from '../screens/agenda';
 import Login from '../screens/login';
 import indicadores from '../screens/indicadores';
-import {NavigationContext} from '@react-navigation/native';
+import { NavigationContext } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
@@ -20,6 +20,7 @@ export default () => {
 
   return (
     <Stack.Navigator>
+<<<<<<< Updated upstream
       {/* <Stack.Screen
         name="login"
         component={Login}
@@ -31,6 +32,19 @@ export default () => {
         component={HomeADM}
         options={{headerShown: false}}
       /> */}
+=======
+      <Stack.Screen
+        name="login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="HomeADM"
+        component={HomeADM}
+        options={{ headerShown: false }}
+      />
+>>>>>>> Stashed changes
 
       {/* <Stack.Screen
         name="Agenda"
@@ -38,34 +52,42 @@ export default () => {
         options={{ headerShown: false }}
       /> */}
 
+<<<<<<< Updated upstream
       {/* <Stack.Screen
         name="HomeGerente"
         component={HomeGerente}
         options={{headerShown: false}}
       /> */}
+=======
+      <Stack.Screen
+        name="HomeGerente"
+        component={HomeGerente}
+        options={{ headerShown: false }}
+      />
+>>>>>>> Stashed changes
 
       <Stack.Screen
         name="Profile"
         component={Profile}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
 
       <Stack.Screen
         name="TarefasAdmin"
         component={TarefasAdmin}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
 
       <Stack.Screen
         name="TarefasGerente"
         component={TarefasGerente}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
 
       <Stack.Screen
         name="Indicadores"
         component={indicadores}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
